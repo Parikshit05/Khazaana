3b689df30c5df6137f137be2b9dfa9032ce2d48a
Parikshit <parikshitpandey03@gmail.com>
2025-08-31 17:36:56 +0530

    changes in env

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = "http://localhost:8000";
-export const BASE_URL = "https://khazaana.onrender.com";
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const API_PATHS = {
     AUTH: {
